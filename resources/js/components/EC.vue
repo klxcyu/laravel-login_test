2263e655ba828072a79e4c4863deb9d5ae3a2b64
klxcyu <klxcyu@gmail.com>
2022-09-14 17:46:24 +0900

    테스트1 브랜치 테스트

--- resources/js/components/EC.vue
+++ resources/js/components/EC.vue
@@ -17,7 +17,7 @@
 <script>
     export default {
         mounted() {
-            console.log('Component mounted.')
+            console.log('Component mounted.');
 
             const users = [
                 { 'user': 'fred', 'age': 48 },
